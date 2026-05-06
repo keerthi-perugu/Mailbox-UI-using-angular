50ff301359d8340006960eee42da3e16854ab9c1
Keerthi Perugu <kperugu@sgp-india.com>
2026-05-06 11:02:56 +0530

    changed the css

--- ng-mailbox/src/app/app.module.ts
+++ ng-mailbox/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { SharedModule } from './shared/shared.module';
 
-// Feature modules (lazy-loaded via router is ideal; for simplicity, direct import here)
 import { EmailListComponent } from './features/mailbox/components/email-list/email-list.component';
 import { EmailCardComponent } from './features/mailbox/components/email-card/email-card.component';
 import { EmailPreviewComponent } from './features/mailbox/components/email-preview/email-preview.component';
